1ae72bf9143e0b62304ef398b2c65d9020994ade
sokanaid <71149930+sokanaid@users.noreply.github.com>
2020-10-16 10:34:30 +0300

    Add files via upload

---
+++ Home work 5/A/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace A
+{
+    
+    class Program
+    {
+        static void Ans()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                for (int j = i + 1; j <= 20; j++)
+                {
+                    for (int k = j + 1; k <= 20; k++)
+                    {
+
+                        if (i * i + j * j == k * k)
+                        {
+                            Console.WriteLine($"{i} {j} {k}");
+                            Console.WriteLine($"{j} {i} {k}");
+                        }
+
+
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Ans();
+        }
+    }
+}
