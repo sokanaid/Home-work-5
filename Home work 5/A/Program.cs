8e8128e4c2515a998968bd620e9c2b20b54a4ba2
sokanaid <71149930+sokanaid@users.noreply.github.com>
2020-10-16 10:55:02 +0300

    Add files via upload

--- Home work 5/A/Program.cs
+++ Home work 5/A/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Threading.Tasks.Dataflow;
 
 namespace A
@@ -6,6 +7,38 @@ namespace A
     
     class Program
     {
+        static long fast(long A, int n,ref bool flag)
+
+        {
+            if (n <= 0)
+            {
+                return 1;
+            }
+            long ans = 1;
+            try
+            {
+                
+                if (n % 2 == 0)
+                {
+                    ans = fast(A, n / 2, ref flag);
+                    if (ans > long.MaxValue / 2) flag = true;
+                    ans *= ans;
+                }
+                else
+                {
+                    long h = fast(A, n / 2, ref flag);
+                    if (fast(A, n / 2, ref flag) > long.MaxValue / A) flag = true;
+                    ans = h*A;
+                }
+            }
+            catch (Exception)
+            {
+                flag = true;
+                
+            }
+            return ans;
+
+        }
         static void Ans()
         {
             for (int i = 1; i <= 20; i++)
@@ -29,6 +62,17 @@ namespace A
         static void Main(string[] args)
         {
             Ans();
+            bool flag = false;
+            long A = 9;
+            int n = 2;
+            long ans=fast(A, n, ref flag);
+            if (flag)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            else {
+                Console.WriteLine(ans);
+            }
         }
     }
 }
